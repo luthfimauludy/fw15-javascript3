2241b6e209ed3e3a64516f4e4dd9d56ea29818f2
Luthfi Putra Mauludy <77185149+luthfimauludy@users.noreply.github.com>
2023-03-24 17:08:18 +0700

    Add files via upload

--- no-4.js
+++ no-4.js
@@ -1,3 +1,4 @@
+// promise 1
 let promise = false;
 
 const Promises = new Promise((resolve, reject) => {
@@ -13,3 +14,20 @@ const Promises = new Promise((resolve, reject) => {
 Promises.then((result) => console.log(result)).catch((error) =>
   console.log(error)
 );
+
+// promise 2
+// function rejectedPromise() {
+//   return new Promise((resolve, reject) => {
+//     reject(new Error("This promise is Rejected!"));
+//   });
+// }
+
+// async function displayData() {
+//   try {
+//     await rejectedPromise();
+//   } catch (error) {
+//     console.log("Error Message:", error.message);
+//   }
+// }
+
+// displayData();
