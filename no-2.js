f72c83c11dacf82a0205bafd69b09d47fe0c9b45
Luthfi Putra Mauludy <77185149+luthfimauludy@users.noreply.github.com>
2023-03-27 09:34:24 +0700

    Add files via upload

--- no-2.js
+++ no-2.js
@@ -19,6 +19,8 @@ cekHariKerja("senin")
   .then((result) => console.log(result))
   .catch((error) => console.log(error.toString()))
   .finally(() => console.log("Selesai"));
+// penggunaan .then() dan .catch() untuk mengatasi error handling yang nantinya jika ada satu code promise yang di reject,
+// maka akan langsung di lempar ke block .catch()
 
 // b
 // (async () => {
@@ -29,3 +31,5 @@ cekHariKerja("senin")
 //     console.log(error.toString());
 //   }
 // })();
+// penggunaan try biasanya kita menyisipkan pada code yang mungkin akan terjadi error,
+// lalu catch akan menangkapnya apabila terjadi suatu error pada block try
